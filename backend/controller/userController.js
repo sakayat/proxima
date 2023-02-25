429736d676597e3cca4cf3ae4aaafe6bcf715794
sakhyat <sakhyatkabir1@gmail.com>
2023-02-25 15:15:59 -0500

    hasing password

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,21 +1,26 @@
+const userModal = require("../models/userModal");
+const User = require("../models/userModal");
+
 // login user
 const loginUser = (req, res) => {
   res.json({ message: "login" });
 };
 
 // signup user
-const signupUser = (req, res) => {
-  res.json({ message: "signup" });
+const signupUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.signup(email,password);
+  res.status(200).json(user);
 };
 
 // users
-
-const getUsers = (req,res) => {
-  res.json({message : "users"})
-}
+const getUsers = (req, res) => {
+  res.json({ message: "users" });
+};
 
 module.exports = {
   loginUser,
   signupUser,
-  getUsers
+  getUsers,
 };
--- backend/models/userModal.js
+++ backend/models/userModal.js
@@ -1,17 +1,36 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcrypt")
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   email: {
     type: String,
     required: true,
-    unique: true,
+    unique: true
   },
   password: {
     type: String,
     required: true,
   },
 });
+
+userSchema.statics.signup = async function(email,password) {
+
+  const exist = await this.findOne({email})
+
+  if(exist){
+    throw Error("Email already exist")
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(password,salt)
+
+  // create user
+  const user = await this.create({
+    email,
+    password : hash
+  })
+  return user
+}
 
 module.exports = mongoose.model("User", userSchema);
