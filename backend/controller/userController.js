d7466b48d72ed29956f09c2c7932af273f0ecf5f
sakhyat <sakhyatkabir1@gmail.com>
2023-02-28 10:16:49 -0500

    token added

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,5 +1,12 @@
-const userModal = require("../models/userModal");
 const User = require("../models/userModal");
+const jwt = require("jsonwebtoken");
+
+// create token
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+};
 
 // login user
 const loginUser = (req, res) => {
@@ -11,7 +18,9 @@ const signupUser = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.signup(email, password);
-    res.status(200).json(user);
+    // create token
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
--- backend/models/userModal.js
+++ backend/models/userModal.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
 const Schema = mongoose.Schema;
-const validator = require("validator")
+const validator = require("validator");
 
 const userSchema = new Schema({
   email: {
@@ -15,33 +15,32 @@ const userSchema = new Schema({
   },
 });
 
-userSchema.statics.signup = async function(email,password) {
-
+userSchema.statics.signup = async function (email, password) {
   // validattion
-  if(!email || !password){
-   throw Error("Email and password are required")
+  if (!email || !password) {
+    throw Error("Email and password are required");
   }
 
   // check if email  and password are valid
-  if(!validator.isEmail(email)){
-    throw Error("Email is invalid")
+  if (!validator.isEmail(email)) {
+    throw Error("Email is invalid");
   }
 
-  if(!validator.isStrongPassword(password)){
-    throw Error("Password not strong, try create password again, including lowercase letters, numbers and underscores")
+  if (!validator.isStrongPassword(password)) {
+    throw Error(
+      "Password not strong, try create password again, including lowercase letters, numbers and underscores"
+    );
   }
 
-  
-
-  const salt = await bcrypt.genSalt(10)
-  const hash = await bcrypt.hash(password,salt)
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
 
   // create user
   const user = await this.create({
     email,
-    password : hash
-  })
-  return user
-}
+    password: hash,
+  });
+  return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
